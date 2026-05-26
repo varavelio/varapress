545f5acb8ac2f088aa71b1e8ba952d73ac5617d9
Luis Eduardo <eduardo.devop@gmail.com>
2026-05-26 17:53:27 +0000

    feat: add download script for Lucide icons with version management

---
+++ static/icons/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION="1.16.0"
+
+rm -rf ./lucide && mkdir -p ./lucide
+wget -q -P ./lucide https://raw.githubusercontent.com/lucide-icons/lucide/refs/tags/$VERSION/LICENSE
+wget -q -P ./lucide https://github.com/lucide-icons/lucide/releases/download/$VERSION/lucide-icons-$VERSION.zip
+unzip -q ./lucide/lucide-icons-$VERSION.zip -d ./lucide
+rm ./lucide/lucide-icons-$VERSION.zip
