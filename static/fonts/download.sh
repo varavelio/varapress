46c9865cbb3d45964e417a10ed79074e96e580c4
Luis Eduardo <eduardo.devop@gmail.com>
2026-05-26 22:33:40 +0000

    feat: add download script for Geist fonts and remove old script

--- static/fonts/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-VERSION="1.7.0"
-
-rm -rf ./geist && mkdir -p ./geist
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/LICENSE.txt
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/dist/fonts/geist-sans/Geist-Variable.woff2
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/dist/fonts/geist-mono/GeistMono-Variable.woff2
