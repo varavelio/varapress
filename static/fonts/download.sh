e839c2de8353d9da9f1e9969a1e8d825523c4cf6
Luis Eduardo <eduardo.devop@gmail.com>
2026-05-26 17:53:20 +0000

    fix: parameterize Geist version in download script for easier updates

--- static/fonts/download.sh
+++ static/fonts/download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+VERSION="1.7.0"
+
 rm -rf ./geist && mkdir -p ./geist
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@1.7.0/LICENSE.txt
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@1.7.0/dist/fonts/geist-sans/Geist-Variable.woff2
-wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@1.7.0/dist/fonts/geist-mono/GeistMono-Variable.woff2
+wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/LICENSE.txt
+wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/dist/fonts/geist-sans/Geist-Variable.woff2
+wget -q -P ./geist https://cdn.jsdelivr.net/npm/geist@$VERSION/dist/fonts/geist-mono/GeistMono-Variable.woff2
