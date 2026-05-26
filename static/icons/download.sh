c7c164705e67385a2ae3f913dfb62460cc24e425
Luis Eduardo <eduardo.devop@gmail.com>
2026-05-26 17:59:30 +0000

    fix: clean up temporary icon directory after extraction

--- static/icons/download.sh
+++ static/icons/download.sh
@@ -7,4 +7,6 @@ rm -rf ./lucide && mkdir -p ./lucide
 wget -q -P ./lucide https://raw.githubusercontent.com/lucide-icons/lucide/refs/tags/$VERSION/LICENSE
 wget -q -P ./lucide https://github.com/lucide-icons/lucide/releases/download/$VERSION/lucide-icons-$VERSION.zip
 unzip -q ./lucide/lucide-icons-$VERSION.zip -d ./lucide
+mv ./lucide/icons/*.svg ./lucide
+rm -rf ./lucide/icons
 rm ./lucide/lucide-icons-$VERSION.zip
